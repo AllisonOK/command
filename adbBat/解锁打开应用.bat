bbb6002be9dc56d1e011663948196f3cf1528d05
lisheng <lisheng@adups.com>
2023-06-07 16:21:27 +0800

    dd

---
+++ adbBat/解锁打开应用.bat
@@ -0,0 +1,8 @@
+adb shell input keyevent 82
+adb shell input mouse tap 100 500
+adb shell input touchscreen swipe 360 1500 360 200
+adb shell am start -n com.easycontrol.emm/.MainActivity
+##adb shell input mouse tap 350 560
+##adb shell input mouse tap 350 670
+
+##adb shell input mouse tap 630 150
---
+++ 抓取日志.bat
@@ -0,0 +1,3 @@
+adb logcat -c
+
+adb logcat | findstr MDM3.3-Log > logcat.txt
